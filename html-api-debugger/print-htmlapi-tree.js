3e854fa42b798f95a0bbbe5edce2314af75e5a8b
Jon Surrell <sirreal@users.noreply.github.com>
2024-05-30 20:22:12 +0200

    types and format overhaul

--- html-api-debugger/print-htmlapi-tree.js
+++ html-api-debugger/print-htmlapi-tree.js
@@ -1,87 +1,75 @@
-export function printHtmlApiTree( node, ul ) {
-	while ( ul.firstChild ) {
-		ul.removeChild( ul.firstChild );
+export function printHtmlApiTree(node, ul) {
+	while (ul.firstChild) {
+		ul.removeChild(ul.firstChild);
 	}
 
-	for ( var i = 0; i < node.childNodes.length; i += 1 ) {
-		const li = document.createElement( 'li' );
-		li.className = 't' + node.childNodes[ i ].nodeType;
-		if ( node.childNodes[ i ].nodeType === 10 ) {
-			li.appendChild( document.createTextNode( 'DOCTYPE: ' ) );
+	for (var i = 0; i < node.childNodes.length; i += 1) {
+		const li = document.createElement('li');
+		li.className = 't' + node.childNodes[i].nodeType;
+		if (node.childNodes[i].nodeType === 10) {
+			li.appendChild(document.createTextNode('DOCTYPE: '));
 		}
-		if ( node.childNodes[ i ].nodeName ) {
-			const code = document.createElement( 'code' );
-			code.appendChild(
-				document.createTextNode( node.childNodes[ i ].nodeName )
-			);
+		if (node.childNodes[i].nodeName) {
+			const code = document.createElement('code');
+			code.appendChild(document.createTextNode(node.childNodes[i].nodeName));
 
-			if ( node.childNodes[ i ].nodeValue ) {
+			if (node.childNodes[i].nodeValue) {
 				code.className = 'hasNodeValue';
 			}
-			li.appendChild( code );
+			li.appendChild(code);
 		} else {
-			const span = document.createElement( 'span' );
-			span.appendChild( document.createTextNode( 'no name' ) );
+			const span = document.createElement('span');
+			span.appendChild(document.createTextNode('no name'));
 			span.className = 'unnamed';
-			li.appendChild( span );
+			li.appendChild(span);
 		}
-		if ( node.childNodes[ i ].nodeValue ) {
-			const el = document.createElement( 'pre' );
+		if (node.childNodes[i].nodeValue) {
+			const el = document.createElement('pre');
 			el.className = 'nodeValue';
-			el.appendChild(
-				document.createTextNode( node.childNodes[ i ].nodeValue )
-			);
-			li.appendChild( el );
+			el.appendChild(document.createTextNode(node.childNodes[i].nodeValue));
+			li.appendChild(el);
 		}
-		if ( node.childNodes[ i ]._span ) {
-			li.dataset.spanStart = node.childNodes[ i ]._span.start;
-			li.dataset.spanLength = node.childNodes[ i ]._span.length;
+		if (node.childNodes[i]._span) {
+			li.dataset.spanStart = node.childNodes[i]._span.start;
+			li.dataset.spanLength = node.childNodes[i]._span.length;
 		}
-		if ( node.childNodes[ i ].attributes ) {
-			for (
-				var j = 0;
-				j < node.childNodes[ i ].attributes.length;
-				j += 1
-			) {
-				if ( node.childNodes[ i ].attributes[ j ].specified ) {
-					const attName = document.createElement( 'code' );
+		if (node.childNodes[i].attributes) {
+			for (var j = 0; j < node.childNodes[i].attributes.length; j += 1) {
+				if (node.childNodes[i].attributes[j].specified) {
+					const attName = document.createElement('code');
 					attName.appendChild(
-						document.createTextNode(
-							node.childNodes[ i ].attributes[ j ].nodeName
-						)
+						document.createTextNode(node.childNodes[i].attributes[j].nodeName),
 					);
 					attName.className = 'attribute name';
-					const attValue = document.createElement( 'code' );
+					const attValue = document.createElement('code');
 					attValue.appendChild(
-						document.createTextNode(
-							node.childNodes[ i ].attributes[ j ].nodeValue
-						)
+						document.createTextNode(node.childNodes[i].attributes[j].nodeValue),
 					);
 					attValue.className = 'attribute value';
-					const att = document.createElement( 'span' );
+					const att = document.createElement('span');
 					att.className = 't2';
-					att.appendChild( attName );
-					att.appendChild( document.createTextNode( '="' ) );
-					att.appendChild( attValue );
-					att.appendChild( document.createTextNode( '"' ) );
-					li.appendChild( document.createTextNode( ' ' ) );
-					li.appendChild( att );
+					att.appendChild(attName);
+					att.appendChild(document.createTextNode('="'));
+					att.appendChild(attValue);
+					att.appendChild(document.createTextNode('"'));
+					li.appendChild(document.createTextNode(' '));
+					li.appendChild(att);
 				}
 			}
 		}
 
-		if ( node.childNodes[ i ].childNodes?.length ) {
-			const ul2 = document.createElement( 'ul' );
-			li.appendChild( ul2 );
-			printHtmlApiTree( node.childNodes[ i ], ul2 );
+		if (node.childNodes[i].childNodes?.length) {
+			const ul2 = document.createElement('ul');
+			li.appendChild(ul2);
+			printHtmlApiTree(node.childNodes[i], ul2);
 		}
-		if ( node.childNodes[ i ].content ) {
-			const ul2 = document.createElement( 'ul' );
-			li.appendChild( ul2 );
+		if (node.childNodes[i].content) {
+			const ul2 = document.createElement('ul');
+			li.appendChild(ul2);
 			ul2.className = 'template';
-			printHtmlApiTree( node.childNodes[ i ].content, ul2 );
+			printHtmlApiTree(node.childNodes[i].content, ul2);
 		}
 
-		ul.appendChild( li );
+		ul.appendChild(li);
 	}
 }
--- html-api-debugger/view.js
+++ html-api-debugger/view.js
@@ -17,16 +17,29 @@ let inFlightRequestAbortController = null;
 /** @type {AbortController|null} */
 let debounceInputAbortController = null;
 
-const { clearSpan, state, render } = I.store( NS, {
+/**
+ * @typedef State
+ * @property {string} formattedHtmlapiResponse
+ *
+ *
+ * @typedef Store
+ * @property {State} state
+ * @property {()=>void} clearSpan
+ */
+
+/** @type {typeof I.store<Store>} */
+const store = I.store;
+
+const { clearSpan, state, render } = store(NS, {
 	state: {
 		get formattedHtmlapiResponse() {
-			return JSON.stringify( state.htmlapiResponse, undefined, 2 );
+			return JSON.stringify(state.htmlapiResponse, undefined, 2);
 		},
 
 		get hoverSpan() {
 			/** @type {string | undefined} */
 			const html = state.htmlapiResponse.result?.html;
-			if ( ! html ) {
+			if (!html) {
 				return '';
 			}
 			return html;
@@ -35,94 +48,97 @@ const { clearSpan, state, render } = I.store( NS, {
 		get hoverSpanSplit() {
 			/** @type {string | undefined} */
 			const html = state.htmlapiResponse.result?.html;
-			if ( ! html || ! state.span ) {
+			if (!html || !state.span) {
 				return [];
 			}
-			const buf = new TextEncoder().encode( html );
+			const buf = new TextEncoder().encode(html);
 			const decoder = new TextDecoder();
 
 			/** @type {{start: number, length: number }} */
 			const { start: spanStart, length } = state.span;
 			const spanEnd = spanStart + length;
 			return [
-				decoder.decode( buf.slice( 0, spanStart ) ),
-				decoder.decode( buf.slice( spanStart, spanEnd ) ),
-				decoder.decode( buf.slice( spanEnd ) ),
+				decoder.decode(buf.slice(0, spanStart)),
+				decoder.decode(buf.slice(spanStart, spanEnd)),
+				decoder.decode(buf.slice(spanEnd)),
 			];
 		},
 	},
 	run() {
-		RENDERED_IFRAME = document.getElementById( 'rendered_iframe' );
+		RENDERED_IFRAME = document.getElementById('rendered_iframe');
 		render();
 
 		// browsers "eat" some characters from search params…
 		// newlines seem especially problematic in chrome
 		// lets clean up the URL
-		const u = new URL( document.location.href );
-		if ( state.html ) {
-			u.searchParams.set( 'html', state.html );
-			history.replaceState( null, '', u );
-		} else if ( u.searchParams.has( 'html' ) ) {
-			u.searchParams.delete( 'html' );
-			history.replaceState( null, '', u );
+		const u = new URL(document.location.href);
+		if (state.html) {
+			u.searchParams.set('html', state.html);
+			history.replaceState(null, '', u);
+		} else if (u.searchParams.has('html')) {
+			u.searchParams.delete('html');
+			history.replaceState(null, '', u);
 		}
 	},
-	onRenderedIframeLoad( e ) {
+	/** @param {Event} e */
+	onRenderedIframeLoad(e) {
+		// @ts-expect-error
 		const doc = e.target.contentWindow.document;
 		state.DOM.renderingMode = doc.compatMode;
 		state.DOM.title = doc.title || '[document has no title]';
 
-		printDOM( document.getElementById( 'dom_tree' ), doc );
+		printDOM(
+			// @ts-expect-error
+			document.getElementById('dom_tree'),
+			doc,
+		);
 	},
 	clearSpan() {
 		state.span = null;
 	},
-	handleChange: function* ( e ) {
+	handleChange: function* (e) {
 		const val = e.target.value;
 
 		state.html = val;
 
-		const u = new URL( document.location.href );
-		u.searchParams.set( 'html', val );
-		history.replaceState( null, '', u );
+		const u = new URL(document.location.href);
+		u.searchParams.set('html', val);
+		history.replaceState(null, '', u);
 
-		debounceInputAbortController?.abort( 'debounced' );
+		debounceInputAbortController?.abort('debounced');
 		debounceInputAbortController = new AbortController();
 		try {
-			yield new Promise( ( resolve, reject ) => {
-				const t = setTimeout( resolve, DEBOUNCE_TIMEOUT );
-				debounceInputAbortController.signal.addEventListener(
-					'abort',
-					() => {
-						clearInterval( t );
-						reject( debounceInputAbortController.signal.reason );
-					}
-				);
-			} );
-		} catch ( e ) {
-			if ( e === 'debounced' ) {
+			yield new Promise((resolve, reject) => {
+				const t = setTimeout(resolve, DEBOUNCE_TIMEOUT);
+				debounceInputAbortController.signal.addEventListener('abort', () => {
+					clearInterval(t);
+					reject(debounceInputAbortController.signal.reason);
+				});
+			});
+		} catch (e) {
+			if (e === 'debounced') {
 				return;
 			}
 			throw e;
 		}
 
-		inFlightRequestAbortController?.abort( 'request superseded' );
+		inFlightRequestAbortController?.abort('request superseded');
 		inFlightRequestAbortController = new AbortController();
 		let resp;
 		try {
-			resp = yield apiFetch( {
-				path: `${ NS }/v1/htmlapi`,
+			resp = yield apiFetch({
+				path: `${NS}/v1/htmlapi`,
 				method: 'POST',
 				data: { html: val },
 				signal: inFlightRequestAbortController.signal,
-			} );
-		} catch ( err ) {
+			});
+		} catch (err) {
 			// We'd like to get this but won't thanks to `apiFetch` hiding the real error.
-			if ( err instanceof DOMException ) {
+			if (err instanceof DOMException) {
 				return;
 			}
 			// `apiFetch` actually does something like this.
-			if ( err && err.code === 'fetch_error' && navigator.onLine ) {
+			if (err && err.code === 'fetch_error' && navigator.onLine) {
 				return;
 			}
 			throw err;
@@ -131,25 +147,25 @@ const { clearSpan, state, render } = I.store( NS, {
 		state.htmlapiResponse = resp;
 		clearSpan();
 
-		if ( resp.error ) {
-			document.getElementById( 'html_api_result_holder' ).innerHTML = '';
+		if (resp.error) {
+			document.getElementById('html_api_result_holder').innerHTML = '';
 			return;
 		}
 
 		printHtmlApiTree(
 			resp.result.tree,
-			document.getElementById( 'html_api_result_holder' )
+			document.getElementById('html_api_result_holder'),
 		);
 	},
 
 	/** @param {MouseEvent} e */
-	handleSpanClick( e ) {
+	handleSpanClick(e) {
 		const t = e.target;
-		if ( t && t instanceof HTMLElement ) {
-			const spanEl = t.closest( '[data-span-start]' );
-			if ( spanEl ) {
-				const start = Number( spanEl.dataset.spanStart );
-				const length = Number( spanEl.dataset.spanLength );
+		if (t && t instanceof HTMLElement) {
+			const spanEl = t.closest('[data-span-start]');
+			if (spanEl) {
+				const start = Number(spanEl.dataset.spanStart);
+				const length = Number(spanEl.dataset.spanLength);
 				state.span = { start, length };
 			}
 		}
@@ -160,15 +176,15 @@ const { clearSpan, state, render } = I.store( NS, {
 	render() {
 		RENDERED_IFRAME.contentWindow.document.open();
 		RENDERED_IFRAME.contentWindow.document.write(
-			'<!DOCTYPE html>\n<html>\n<body>' + state.html
+			'<!DOCTYPE html>\n<html>\n<body>' + state.html,
 		);
 		RENDERED_IFRAME.contentWindow.document.close();
 
-		if ( state.htmlapiResponse.result?.tree ) {
+		if (state.htmlapiResponse.result?.tree) {
 			printHtmlApiTree(
 				state.htmlapiResponse.result.tree,
-				document.getElementById( 'html_api_result_holder' )
+				document.getElementById('html_api_result_holder'),
 			);
 		}
 	},
-} );
+});
