32ec32aac459ecd1fe81819e32b029eeffb47186
Jon Surrell <sirreal@users.noreply.github.com>
2024-11-08 11:39:04 +0100

    Prep for 2.2

--- html-api-debugger/html-api-debugger.php
+++ html-api-debugger/html-api-debugger.php
@@ -3,9 +3,9 @@
  * Plugin Name:       HTML API Debugger
  * Plugin URI:        https://github.com/sirreal/html-api-debugger
  * Description:       Add a page to wp-admin for debugging the HTML API.
- * Version:           2.1
+ * Version:           2.2
  * Requires at least: 6.6
- * Tested up to:      6.7
+ * Tested up to:      6.8
  * Author:            Jon Surrell
  * Author URI:        https://profiles.wordpress.org/jonsurrell/
  * License:           GPLv2 or later
@@ -22,7 +22,7 @@ use Exception;
 require_once __DIR__ . '/html-api-integration.php';
 
 const SLUG    = 'html-api-debugger';
-const VERSION = '2.1';
+const VERSION = '2.2';
 
 /** Set up the plugin. */
 function init() {
--- html-api-debugger/readme.txt
+++ html-api-debugger/readme.txt
@@ -2,14 +2,18 @@
 Contributors: jonsurrell
 Tags: HTML API, development, debug
 Requires at least: 6.6
-Tested up to: 6.7
-Stable tag: 2.1
+Tested up to: 6.8
+Stable tag: 2.2
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
 Add a page to wp-admin for debugging the HTML API.
 
 == Changelog ==
+
+= 2.2 =
+* Drop basic fragment parsing and quirks mode support.
+* Add advanced context and fragment parsing when supported (WordPress 6.8+).
 
 = 2.1 =
 * Add text-wrap styling fallback for unsupported browsers.
